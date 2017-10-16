685a82145bc5b6235042cd573f75375a8d5222df
Marco Caccin <marco.caccin@gmail.com>
2017-10-16 16:30:02 +0200

    `crop_image` can output images of any shape and use segmentation models of any input shape

--- crop_image.py
+++ crop_image.py
@@ -1,55 +1,57 @@
-import os
 import sys
 
 import numpy as np
-
+import os
 from PIL import Image
-
 from keras.models import load_model
 
 
-def crop_image(imgname, model=None):   
-
+def crop_image(imgname, model=None, out_size=(224, 224)):
     if isinstance(model, str):
         model = load_model(model)
-
+        
+    model_size = model.input_shape[1:-1]
+    
     img0 = Image.open(imgname)
-    xsize, ysize = img0.size
+    x_size, y_size = img0.size
 
-    img = img0.resize((512, 512))
-    img = np.array(img.convert('L'))
+    # Convert input image into square BW image
+    # with luminance values in range [-1, 1]
+    img = img0.resize(model_size)
+    img = np.array(img.convert("L"))
     img = 2 * ((img / 255.) - 0.5)
     
-    pred = model.predict(img[None,:,:,None])[0,:,:,0]
+    # Predict pixel-wise probabilities and convert into luminance
+    pred = model.predict(img[None, :, :, None])[0, :, :, 0]
     pred = (255 * pred).astype(np.uint8)
-
+    
+    # Resize image to initial size
     pred = np.array(Image.fromarray(pred).resize(img0.size))
-
+    
     pos = np.array(np.where(pred > 200))
-
+    
     medians = np.median(pos, axis=1).astype(int)
-    std = 2 * int(max(np.std(pos, axis=1))) + int(0.1 * min(img0.size))
-
-    ymin, xmin = medians - std
-    ymax, xmax = medians + std
-              
+    std = 2 * int(max(np.std(pos, axis=1))) + int(0.1 * min(x_size, y_size))
+    
+    y_min, x_min = medians - std
+    y_max, x_max = medians + std
+    
     out = img0.crop([
-        max(0, xmin), # left
-        max(0, ymin), # upper
-        min(xsize, xmax), # right
-        min(ysize, ymax) # lower
+        max(0, x_min),  # left
+        max(0, y_min),  # upper
+        min(x_size, x_max),  # right
+        min(y_size, y_max)  # lower
     ])
+    
+    return out.resize(out_size)
 
-    return out.resize((224,224))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     modelname = sys.argv[1].strip()
     imgname = sys.argv[2].strip()
-
+    
     out = crop_image(imgname, model=modelname)
-
+    
     u = os.path.split(modelname)[-1]
     v = os.path.split(imgname)[-1]
-    out.save('predictions/pred-{}-{}.png'.format(u, v))
-
+    out.save("predictions/pred-{}-{}.jpg".format(u, v))
