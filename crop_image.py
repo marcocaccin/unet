47c31d2a931e5df5ed888c5ab885c528b2be7f06
Marco Caccin <marco.caccin@gmail.com>
2018-02-12 09:45:07 +0100

    Better cropping

--- crop_image.py
+++ crop_image.py
@@ -1,3 +1,4 @@
+import glob
 import sys
 
 import numpy as np
@@ -6,7 +7,7 @@ from PIL import Image
 from keras.models import load_model
 
 
-def crop_image(imgname, model=None, out_size=(224, 224)):
+def crop_image(imgname, model, threshold=200, out_size=None):
     if isinstance(model, str):
         model = load_model(model)
         
@@ -28,7 +29,7 @@ def crop_image(imgname, model=None, out_size=(224, 224)):
     # Resize image to initial size
     pred = np.array(Image.fromarray(pred).resize(img0.size))
     
-    pos = np.array(np.where(pred > 200))
+    pos = np.array(np.where(pred > threshold))
     
     medians = np.median(pos, axis=1).astype(int)
     std = 2 * int(max(np.std(pos, axis=1))) + int(0.1 * min(x_size, y_size))
@@ -36,22 +37,36 @@ def crop_image(imgname, model=None, out_size=(224, 224)):
     y_min, x_min = medians - std
     y_max, x_max = medians + std
     
-    out = img0.crop([
+    cropped = img0.crop([
         max(0, x_min),  # left
         max(0, y_min),  # upper
         min(x_size, x_max),  # right
         min(y_size, y_max)  # lower
     ])
+
+    mask = pred > threshold
+    masked = np.array(img0) * mask[:, :, None]
+    masked = Image.fromarray(masked)
     
-    return out.resize(out_size)
+    if out_size:
+        cropped = cropped.resize(out_size)
+        masked = masked.resize(out_size)
+
+    return cropped, masked
 
 
 if __name__ == "__main__":
     modelname = sys.argv[1].strip()
-    imgname = sys.argv[2].strip()
     
-    out = crop_image(imgname, model=modelname)
+    model = load_model(modelname)
     
-    u = os.path.split(modelname)[-1]
-    v = os.path.split(imgname)[-1]
-    out.save("predictions/pred-{}-{}.jpg".format(u, v))
+    for imgname in glob.iglob('../openfriday_nn/Test/*.png'):
+        try:
+            cropped, mask = crop_image(imgname=imgname, model=model)
+            
+            u = os.path.split(modelname)[-1]
+            v = os.path.split(imgname)[-1]
+            # cropped.save("predictions/pred-{}-{}.jpg".format(u, v))
+            mask.save("predictions/pred-{}-{}.jpg".format(u, v))
+        except Exception as err:
+            print(err)
